187478a873f9606384cd47d490a0d1296a812109
Radek <radek.goralik@interia.pl>
2026-06-07 13:46:14 +0200

    refactor V2__insert_data.sql

--- region-analytics-service/src/main/resources/db/migration/V2__insert_data.sql
+++ region-analytics-service/src/main/resources/db/migration/V2__insert_data.sql
@@ -1,17 +1,14 @@
--- 1. Wstawiamy kategorie i pobieramy ich wygenerowane ID
-INSERT INTO bdl_variables (api_name) VALUES ('crimes');
-INSERT INTO bdl_variables (api_name) VALUES ('population_in_thousands');
-INSERT INTO bdl_variables (api_name) VALUES ('population_density');
-INSERT INTO bdl_variables (api_name) VALUES ('unemployment');
-INSERT INTO bdl_variables (api_name) VALUES ('gross_salary');
+INSERT INTO bdl_variables (api_name)
+VALUES ('crimes'),
+       ('population_in_thousands'),
+       ('population_density'),
+       ('unemployment'),
+       ('gross_salary');
 
--- 2. Wstawiamy poszczególne ID powiązane z kategoriami
--- Dla crimes (zakładając id = 1):
-INSERT INTO bdl_variable_ids (bdl_variable_id, bdl_id) VALUES (1, 58559);
-INSERT INTO bdl_variable_ids (bdl_variable_id, bdl_id) VALUES (1, 1749155);
-
--- Dla reszty analogicznie (id = 2, 3, 4, 5):
-INSERT INTO bdl_variable_ids (bdl_variable_id, bdl_id) VALUES (2, 1645341);
-INSERT INTO bdl_variable_ids (bdl_variable_id, bdl_id) VALUES (3, 60559);
-INSERT INTO bdl_variable_ids (bdl_variable_id, bdl_id) VALUES (4, 60270);
-INSERT INTO bdl_variable_ids (bdl_variable_id, bdl_id) VALUES (5, 64428);
+INSERT INTO bdl_variable_ids (bdl_variable_id, bdl_id)
+VALUES ((SELECT id FROM bdl_variables WHERE api_name = 'crimes'), 58559),
+       ((SELECT id FROM bdl_variables WHERE api_name = 'crimes'), 1749155),
+       ((SELECT id FROM bdl_variables WHERE api_name = 'population_in_thousands'), 1645341),
+       ((SELECT id FROM bdl_variables WHERE api_name = 'population_density'), 60559),
+       ((SELECT id FROM bdl_variables WHERE api_name = 'unemployment'), 60270),
+       ((SELECT id FROM bdl_variables WHERE api_name = 'gross_salary'), 64428);
