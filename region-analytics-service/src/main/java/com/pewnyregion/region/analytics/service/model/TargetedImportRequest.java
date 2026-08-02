6b5c08e4a3d911ed0745ea268b087ad8e2a4e7d2
Radek <radek.goralik@interia.pl>
2026-08-02 11:50:49 +0200

    add more test cases and valid annotation

--- region-analytics-service/src/main/java/com/pewnyregion/region/analytics/service/model/TargetedImportRequest.java
+++ region-analytics-service/src/main/java/com/pewnyregion/region/analytics/service/model/TargetedImportRequest.java
@@ -1,8 +1,22 @@
 package com.pewnyregion.region.analytics.service.model;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
 import java.util.List;
 
 public record TargetedImportRequest (
+        @NotEmpty(message = "apiNames cannot be empty")
+        @Size(max = 10, message = "Maximum 10 variables allowed for a single import")
         List<String> apiNames,
-        List<Integer> years) {
+
+        @NotEmpty(message = "years cannot be empty")
+        List<@NotNull(message = "year cannot be null")
+        @Min(value = 2012, message = "year cannot be earlier than 2012")
+        @Max(value = 2026, message = "year cannot be later than 2026")
+                Integer> years
+) {
 }
