263e2f705d3cb71b77d93bdb6e12033d418df3ef
Radgor144 <106109773+Radgor144@users.noreply.github.com>
2026-08-02 11:55:54 +0200

    Add new ImportJobControllerTest (#24)
    * Add new ImportJobControllerTest
    
    * add more test cases and valid annotation

--- region-analytics-service/src/test/java/com/pewnyregion/region/analytics/service/utils/TestConstants.java
+++ region-analytics-service/src/test/java/com/pewnyregion/region/analytics/service/utils/TestConstants.java
@@ -10,4 +10,10 @@ public class TestConstants {
 
     public static final String GET_VARIABLES_API_PATH = "/api/variables";
     public static final String GET_MAP_COUNTY_SCORES_API_PATH = "/api/map/county-scores";
+
+    public static final String BASE_IMPORT_PATH = "/api/imports";
+    public static final String POST_TERYT_IMPORT_API_PATH = BASE_IMPORT_PATH + "/teryt";
+    public static final String POST_FULL_IMPORT_API_PATH = BASE_IMPORT_PATH + "/full";
+    public static final String POST_TARGETED_IMPORT_API_PATH = BASE_IMPORT_PATH + "/targeted";
+
 }
